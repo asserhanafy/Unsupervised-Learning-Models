9340ef277a3c0a47e801b081da579912b99510e9
Asser Hanafy <asserhanafy68@gmail.com>
2025-12-24 15:00:55 +0200

    Create Kmeans.py

---
+++ Kmeans.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+class KMeans:
+    def __init__(self, k, max_iter=300, tol=1e-4, init="kmeans++"):
+        self.k = k
+        self.max_iter = max_iter
+        self.tol = tol
+        self.init = init
+
+    def _init_random(self, X):
+        idx = np.random.choice(len(X), self.k, replace=False)
+        return X[idx]
+
+    def _init_kmeanspp(self, X):
+        centroids = [X[np.random.randint(len(X))]]
+        for _ in range(1, self.k):
+            dist = np.min(
+                np.linalg.norm(X[:, None] - np.array(centroids), axis=2) ** 2,
+                axis=1
+            )
+            probs = dist / np.sum(dist)
+            centroids.append(X[np.random.choice(len(X), p=probs)])
+        return np.array(centroids)
+
+    def fit(self, X):
+        self.centroids_ = (
+            self._init_kmeanspp(X) if self.init == "kmeans++"
+            else self._init_random(X)
+        )
+
+        self.inertia_history_ = []
+
+        for _ in range(self.max_iter):
+            dist = np.linalg.norm(X[:, None] - self.centroids_, axis=2)
+            labels = np.argmin(dist, axis=1)
+
+            new_centroids = np.array([
+                X[labels == i].mean(axis=0) for i in range(self.k)
+            ])
+
+            inertia = np.sum((X - new_centroids[labels]) ** 2)
+            self.inertia_history_.append(inertia)
+
+            if np.linalg.norm(new_centroids - self.centroids_) < self.tol:
+                break
+
+            self.centroids_ = new_centroids
+
+        self.labels_ = labels
+        self.inertia_ = self.inertia_history_[-1]
